4d80100bf9dc9aefc9df66ad8c48455e2fef8619
ShowGa <showgacareer@gmail.com>
2024-07-10 23:36:49 +0800

    Progress : Files setup

---
+++ client/src/pages/index.js
@@ -0,0 +1,8 @@
+import CategoryPage from "./CategoryPage";
+import Editor from "./Editor";
+import Home from "./Home";
+import Login from "./Login";
+import Post from "./Post";
+import Search from "./Search";
+
+export { CategoryPage, Editor, Home, Login, Post, Search };
