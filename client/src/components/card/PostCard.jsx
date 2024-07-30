eb20dc39f135d032b8bd3a656b33f53c9c8ad5a1
ShowGa <showgacareer@gmail.com>
2024-07-30 14:21:35 +0800

    <Modify> Post card <Link>

--- client/src/components/card/PostCard.jsx
+++ client/src/components/card/PostCard.jsx
@@ -6,22 +6,26 @@ import { postTest, gao } from "../../assets";
 
 const PostCard = () => {
   return (
-    <div className="c-postcard_container">
-      <div className="c-postcard_img_container">
-        <img src={gao} className="c-img_hover-effect" />
-      </div>
+    <div>
+      <Link className="c-postcard_container" to={"/post/123"}>
+        <div className="c-postcard_img_container">
+          <img src={gao} className="c-img_hover-effect" />
+        </div>
 
-      <div className="c-postcard_text_container">
-        <p className="text-sm">2024-07-23</p>
-        <h1 className="text-2xl font-bold max-sm:text-xl">How to Write Code</h1>
-        <p className="text-sm line-clamp-2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
-          tempora quam inventore sunt. Vero quam cum hic quis delectus est,
-          doloremque tenetur similique accusamus impedit, perferendis aperiam
-          labore eveniet voluptatum.
-        </p>
-        <span className="c-tag_text-color">Code</span>
-      </div>
+        <div className="c-postcard_text_container">
+          <p className="text-sm">2024-07-23</p>
+          <h1 className="text-2xl font-bold max-sm:text-xl">
+            How to Write Code
+          </h1>
+          <p className="text-sm line-clamp-2">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Expedita
+            tempora quam inventore sunt. Vero quam cum hic quis delectus est,
+            doloremque tenetur similique accusamus impedit, perferendis aperiam
+            labore eveniet voluptatum.
+          </p>
+          <span className="c-tag_text-color">Code</span>
+        </div>
+      </Link>
     </div>
   );
 };
--- client/src/components/card/SideBarPostCard.jsx
+++ client/src/components/card/SideBarPostCard.jsx
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 // Image
 import { postTest, gao } from "../../assets";
 
 const SideBarPostCard = () => {
   return (
-    <div className="c-sideBarPostCard_container">
-      <div className="c-sideBarPostCard_img_container">
-        <img src={gao} alt="" className="c-img_hover-effect" />
-      </div>
+    <div>
+      <Link className="c-sideBarPostCard_container" to={"/post/123"}>
+        <div className="c-sideBarPostCard_img_container">
+          <img src={gao} alt="" className="c-img_hover-effect" />
+        </div>
 
-      <div className="c-sideBarPostCard_text_container">
-        <span className="c-tag_text-color">Food</span>
+        <div className="c-sideBarPostCard_text_container">
+          <span className="c-tag_text-color">Food</span>
 
-        <h1 className="text-lg font-bold leading-5">
-          How to eat shiiit without feeling to barf
-        </h1>
+          <h1 className="text-lg font-bold leading-5">
+            How to eat shiiit without feeling to barf
+          </h1>
 
-        <p className="text-[10px]">23.07.2024</p>
-      </div>
+          <p className="text-[10px]">23.07.2024</p>
+        </div>
+      </Link>
     </div>
   );
 };
