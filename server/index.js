233728cee13175b5982685c72f5c4dd14577bfb4
ShowGa <showgacareer@gmail.com>
2024-08-06 12:28:48 +0800

    <Progress> Backend index.js setup (Connecting database, create server)

--- server/index.js
+++ server/index.js
@@ -1,4 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
+dotenv.config();
 const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION || "mongodb://127.0.0.1/showgablogDB")
+  .then(() => {
+    console.log("Successfully connect to MongoDB");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Sup bitchh !");
+});
+
+app.listen(8080, () => {
+  console.log("Server listening to Port 8080");
+});
