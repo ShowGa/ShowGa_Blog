f5f1b8a4c386e1dbd6c21049206946595cb15b5d
ShowGa <showgacareer@gmail.com>
2024-08-06 17:45:03 +0800

    <Modify> Modify Post Model

--- server/models/postModel.js
+++ server/models/postModel.js
@@ -1,34 +1,37 @@
 import mongoose from "mongoose";
 
-const postSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const postSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    banerImg: {
+      type: String,
+      default: "",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    views: {
+      type: Number,
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    belongAuthorID: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
   },
-  banerImg: {
-    type: String,
-    default: "",
-  },
-  content: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-  views: {
-    type: Number,
-  },
-  slug: {
-    type: String,
-    required: true,
-  },
-  belongAuthorID: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Post = mongoose.model("Post", postSchema);
 
