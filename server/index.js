4aba487f8131e559ab19ee5e03ddbeee499d9587
ShowGa <showgacareer@gmail.com>
2024-08-08 23:21:31 +0800

    <Progress> Client: handleGoogleLogin functionality Server: index.js change the cors() config

--- server/index.js
+++ server/index.js
@@ -18,7 +18,12 @@ mongoose
     console.log(e);
   });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(cookieParser());
 app.use(express.static("public"));
 app.use(express.json());
