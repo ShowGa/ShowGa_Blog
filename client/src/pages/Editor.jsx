bc2ed90d8437e4a0650e8f12cf728c82505827f6
ShowGa <showgacareer@gmail.com>
2024-08-11 18:21:03 +0800

    <Modify> Client: Editor.jsx add img functionality

--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -8,12 +8,66 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.bubble.css";
 // CSS
 import "./pages.css";
+import toast from "react-hot-toast";
+// firebase
+import {
+  getDownloadURL,
+  getStorage,
+  uploadBytesResumable,
+  ref,
+} from "firebase/storage";
+// firebase config
+import { app } from "../firebase/firebase";
 
 const Editor = () => {
   const { t } = useTranslation();
 
   const [editorContent, setEditorContent] = useState("");
   const [plusBtnShow, setPlusBtnShow] = useState(false);
+
+  const handleImgBtnClick = () => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+    input.onchange = () => {
+      const file = input.files[0];
+
+      console.log(file);
+
+      if (file) {
+        console.log("uploading");
+        uploadImg(file);
+      }
+    };
+  };
+
+  const uploadImg = (file) => {
+    const storage = getStorage(app);
+    // add random info to specialize the file name
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          genrateImgElement(downloadURL);
+        });
+      }
+    );
+  };
+
+  const genrateImgElement = (downloadURL) => {
+    let imgContent;
+    imgContent = `<img src="${downloadURL}"/>`;
+
+    setEditorContent(editorContent + imgContent);
+  };
 
   return (
     <div className="max-w-[50%] min-h-[100vh] mx-auto max-md:max-w-[95%] pt-11">
@@ -39,7 +93,7 @@ const Editor = () => {
           />
         </button>
         {plusBtnShow && (
-          <div className="p-btn_container">
+          <div onClick={handleImgBtnClick} className="p-btn_container">
             <button>
               <AiOutlinePicture className="p-function_btn" />
             </button>
@@ -57,7 +111,7 @@ const Editor = () => {
 
 const modules = {
   toolbar: [
-    [{ header: "1" }, { header: "2" }],
+    [{ header: "1" }, { header: "2" }, "image"],
     ["bold", "italic", "underline"],
     ["clean"],
   ],
