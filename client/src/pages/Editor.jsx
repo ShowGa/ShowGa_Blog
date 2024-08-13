e405080889268f5f3e89c4ef44febf79b163af79
ShowGa <showgacareer@gmail.com>
2024-08-13 15:38:14 +0800

    <Modify> Client: Editor.jsx 1. onChange postContent 2. upload baner image funtionality

--- client/src/pages/Editor.jsx
+++ client/src/pages/Editor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 // React icons
 import { CiCirclePlus } from "react-icons/ci";
@@ -17,12 +17,29 @@ import {
 } from "firebase/storage";
 // firebase config
 import { app } from "../firebase/firebase";
+// constants
+import { tagInfo } from "../constants";
+import toast from "react-hot-toast";
 
 const Editor = () => {
   const { t } = useTranslation();
 
   const [editorContent, setEditorContent] = useState("");
   const [plusBtnShow, setPlusBtnShow] = useState(false);
+  const [postContent, setPostContent] = useState({
+    title: "",
+    banerImg: "",
+    content: "",
+    category: tagInfo[0].tagName,
+    isFeature: false,
+  });
+
+  const handleChange = (e) => {
+    setPostContent({
+      ...postContent,
+      [e.target.id]: e.target.value,
+    });
+  };
 
   const handleImgBtnClick = () => {
     const input = document.createElement("input");
@@ -32,10 +49,7 @@ const Editor = () => {
     input.onchange = () => {
       const file = input.files[0];
 
-      console.log(file);
-
       if (file) {
-        console.log("uploading");
         uploadImg(file);
       }
     };
@@ -51,11 +65,47 @@ const Editor = () => {
       "state_changed",
       (snapshot) => {},
       (error) => {
+        toast.error(
+          "Error occurred when uploading image ! \n Please check the file, file size must be less than 2MB ."
+        );
         console.log(error);
       },
       () => {
         getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
           genrateImgElement(downloadURL);
+        });
+      }
+    );
+  };
+
+  // handle baner img file change
+  const handleBanerImgChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      uploadBanerImg(file);
+    }
+  };
+  // uploadBanerImg
+  const uploadBanerImg = (file) => {
+    const storage = getStorage(app);
+    // add random info to specialize the file name
+    const fileName = new Date().getTime() + file.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytesResumable(storageRef, file);
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        toast.error(
+          "Error occurred when uploading image ! \n Please check the file, file size must be less than 2MB ."
+        );
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+          setPostContent({ ...postContent, banerImg: downloadURL });
+          toast.success("Upload img successfully !");
         });
       }
     );
@@ -68,9 +118,59 @@ const Editor = () => {
     setEditorContent(editorContent + imgContent);
   };
 
+  useEffect(() => {
+    setPostContent({
+      ...postContent,
+      content: editorContent,
+    });
+  }, [editorContent]);
+
   return (
     <div className="max-w-[50%] min-h-[100vh] mx-auto max-md:max-w-[95%] pt-11">
-      <input type="text" placeholder="Title" className="p-editor_title_input" />
+      <input
+        onChange={handleChange}
+        id="title"
+        type="text"
+        placeholder="Title"
+        className="p-editor_title_input"
+      />
+
+      <div className="p-editor_banerImg_container">
+        <span>Baner Image : </span>
+        <input
+          onChange={handleBanerImgChange}
+          id="banerImg"
+          type="file"
+          placeholder="Baner Img"
+        />
+      </div>
+
+      <div className="p-select_container">
+        <div>
+          <label className="font-bold">Featured :</label>
+          <select onChange={handleChange} name="Featured" id="isFeature">
+            <option value="false">False</option>
+            <option value="true">True</option>
+          </select>
+        </div>
+
+        <div>
+          <label className="font-bold">Category :</label>
+          <select onChange={handleChange} name="Category" id="category">
+            {tagInfo.map((tag) => {
+              return (
+                <option
+                  value={tag.tagName}
+                  className={`${tag.bgColor}`}
+                  key={tag.tagName}
+                >
+                  {tag.tagName}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+      </div>
 
       <ReactQuill
         theme="bubble"
