5637251ab69c0ad04a4c3bba47c8f22fae89ebfd
ShowGa <peter1997217123@gmail.com>
2024-08-15 09:13:19 +0800

    <Modify> Client: Modify the structure of Post page

--- client/src/pages/Post.jsx
+++ client/src/pages/Post.jsx
@@ -43,104 +43,108 @@ const Post = () => {
   return (
     <div className="max-w-[80%] mx-auto max-md:max-w-[95%]">
       {/* Hero section */}
-      <section>
-        <div className="p-post_page_heroSection_container">
-          <div className="p-post_title_container">
-            <h1>{post && post.title}</h1>
-            <div className="p-author_container">
-              <img src={gao} />
-              <div className="p-author_text">
-                <p>ShowGa Hsiao</p>
-                <span>{post && post.createdAt}</span>
+      {post && (
+        <>
+          <section>
+            <div className="p-post_page_heroSection_container">
+              <div className="p-post_title_container">
+                <h1>{post && post.title}</h1>
+                <div className="p-author_container">
+                  <img src={gao} />
+                  <div className="p-author_text">
+                    <p>ShowGa Hsiao</p>
+                    <span>{post && post.createdAt}</span>
+                  </div>
+                </div>
+
+                <div className="p-post_title_function-list">
+                  <div className="p-function_clap_container">
+                    <PiHandsClapping className="text-xl" />
+                    <p>6969</p>
+                  </div>
+
+                  <div
+                    className="p-function_comments_container"
+                    onClick={() => {
+                      setShowCommentSec(!showCommentSec);
+                    }}
+                  >
+                    <FaRegComment className="text-xl" />
+                    <p>50</p>
+                  </div>
+                </div>
+              </div>
+
+              <div className="p-post_img_container">
+                <img src={post && post.banerImg} />
               </div>
             </div>
+          </section>
 
-            <div className="p-post_title_function-list">
-              <div className="p-function_clap_container">
-                <PiHandsClapping className="text-xl" />
-                <p>6969</p>
-              </div>
-
-              <div
-                className="p-function_comments_container"
-                onClick={() => {
-                  setShowCommentSec(!showCommentSec);
-                }}
-              >
-                <FaRegComment className="text-xl" />
-                <p>50</p>
-              </div>
-            </div>
-          </div>
-
-          <div className="p-post_img_container">
-            <img src={post && post.banerImg} />
-          </div>
-        </div>
-      </section>
-
-      <section
-        className={`p-post_comments_section ${
-          showCommentSec ? "p-comments_section-effect" : ""
-        }`}
-        onClick={() => {
-          setShowCommentSec(!showCommentSec);
-        }}
-      >
-        <div
-          className="p-post_comments_container"
-          onClick={(e) => {
-            e.stopPropagation();
-          }}
-        >
-          <div className="p-row_flex_between">
-            <p className="text-xl font-bold">Comments</p>
-            <FaWindowClose
-              onClick={() => {
-                setShowCommentSec(!showCommentSec);
-              }}
-              className="text-2xl cursor-pointer"
-            />
-          </div>
-
-          <div className="p-textarea_container">
-            {authUser ? (
-              <textarea name="" id=""></textarea>
-            ) : (
-              <div>
-                <p>
-                  <Link to={"/login"} className="link">
-                    Login
-                  </Link>{" "}
-                  to leave comments !
-                </p>
-              </div>
-            )}
-          </div>
-
-          <div>
-            <CommentCard />
-            <CommentCard />
-            <CommentCard />
-          </div>
-        </div>
-      </section>
-
-      {/* Post and Sidebar section */}
-      <section className="p-post-sidebarSec">
-        <div className="p-post-sidebarSec_container">
-          <div className="p-article_container">
+          <section
+            className={`p-post_comments_section ${
+              showCommentSec ? "p-comments_section-effect" : ""
+            }`}
+            onClick={() => {
+              setShowCommentSec(!showCommentSec);
+            }}
+          >
             <div
-              className="p-post"
-              dangerouslySetInnerHTML={{ __html: post && post.content }}
-            ></div>
-          </div>
+              className="p-post_comments_container"
+              onClick={(e) => {
+                e.stopPropagation();
+              }}
+            >
+              <div className="p-row_flex_between">
+                <p className="text-xl font-bold">Comments</p>
+                <FaWindowClose
+                  onClick={() => {
+                    setShowCommentSec(!showCommentSec);
+                  }}
+                  className="text-2xl cursor-pointer"
+                />
+              </div>
 
-          <aside className="max-lg:hidden">
-            <RecommendSideBar />
-          </aside>
-        </div>
-      </section>
+              <div className="p-textarea_container">
+                {authUser ? (
+                  <textarea name="" id=""></textarea>
+                ) : (
+                  <div>
+                    <p>
+                      <Link to={"/login"} className="link">
+                        Login
+                      </Link>{" "}
+                      to leave comments !
+                    </p>
+                  </div>
+                )}
+              </div>
+
+              <div>
+                <CommentCard />
+                <CommentCard />
+                <CommentCard />
+              </div>
+            </div>
+          </section>
+
+          {/* Post and Sidebar section */}
+          <section className="p-post-sidebarSec">
+            <div className="p-post-sidebarSec_container">
+              <div className="p-article_container">
+                <div
+                  className="p-post"
+                  dangerouslySetInnerHTML={{ __html: post && post.content }}
+                ></div>
+              </div>
+
+              <aside className="max-lg:hidden">
+                <RecommendSideBar />
+              </aside>
+            </div>
+          </section>
+        </>
+      )}
     </div>
   );
 };
