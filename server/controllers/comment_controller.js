6ab055c65e85ea65634d4f8d8fadfc903819d2a5
ShowGa <peter1997217123@gmail.com>
2024-08-18 10:36:08 +0800

    <Progress> Server: Create getPostComments route

--- server/controllers/comment_controller.js
+++ server/controllers/comment_controller.js
@@ -1,4 +1,5 @@
 import Comment from "../models/commentModel.js";
+import Post from "../models/postModel.js";
 
 export const createComment = async (req, res) => {
   try {
@@ -22,5 +23,34 @@ export const createComment = async (req, res) => {
     res
       .status(500)
       .json({ error: "Internal server error ! Please contact us ." });
+  }
+};
+
+export const getPostComments = async (req, res) => {
+  try {
+    const { belongPostID } = req.params;
+    const queryContent = req.query;
+
+    const startIndex = parseInt(queryContent) || 0;
+    const limit = parseInt(queryContent.limit) || 8;
+    const sort = queryContent.sort || "createdAt";
+    const order = queryContent.order || "desc";
+
+    const foundComments = await Comment.find({ belongPostID })
+      .sort({ [sort]: order })
+      .skip(startIndex)
+      .limit(limit);
+
+    const totalComments = await Post.countDocuments({
+      belongPostID,
+    });
+
+    // check is there any more post
+    const isEnd = startIndex + limit >= totalComments;
+
+    return res.status(200).json({ foundComments, isEnd });
+  } catch (e) {
+    console.log("Error in getPostComments controller !" + e);
+    return res.status(500).json({ error: "Internal server error !" });
   }
 };
--- server/routes/comment_route.js
+++ server/routes/comment_route.js
@@ -1,9 +1,14 @@
 import express from "express";
 import { verifyUser } from "../utils/verifyUser.js";
-import { createComment } from "../controllers/comment_controller.js";
+import {
+  createComment,
+  getPostComments,
+} from "../controllers/comment_controller.js";
 
 const router = express.Router();
 
 router.post("/create", verifyUser, createComment);
+
+router.get("/getpostcomments/:belongPostID", getPostComments);
 
 export default router;
