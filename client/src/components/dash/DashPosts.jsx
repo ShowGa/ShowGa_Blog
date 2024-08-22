f91ec4ab9f4c30a6bbede0c696bca1164648b9b5
ShowGa <peter1997217123@gmail.com>
2024-08-22 16:18:27 +0800

    <Progress> Client: DashPost.jsx => search post for modifying functionality and some CSS

--- client/src/components/dash/DashPosts.jsx
+++ client/src/components/dash/DashPosts.jsx
@@ -1,21 +1,58 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 // css
 import "../components.css";
 // components
 import PostCard from "../card/PostCard";
+// service
+import PostService from "../../services/post-service.js";
+// hot toast
+import toast from "react-hot-toast";
+import { Link } from "react-router-dom";
 
 const DashPosts = () => {
   const [post, setPost] = useState(null);
+  const searchData = useRef();
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+
+    if (searchData.current.value === "") {
+      toast.error("Type some words !");
+      return;
+    }
+
+    const searchQuery = `?searchTerm=${searchData.current.value}`;
+
+    PostService.getAllPosts(searchQuery)
+      .then((res) => {
+        setPost(res.data.foundPost);
+      })
+      .catch((e) => {
+        toast.error("Error occurred when searching posts !");
+        console.log(e);
+      });
+  };
 
   return (
-    <main className="max-w-[80%] mx-auto max-md:max-w-[95%] mt-12">
-      <section>
-        <form className="c-dash-search_form">
-          <label>Search Term :</label>
-          <input id="searchTerm" type="text" />
+    <main className="w-[80%] mx-auto mt-12">
+      <section className="w-full">
+        <form onSubmit={handleSubmitForm} className="c-dash-search_form">
+          <div>
+            <label>Search Term :</label>
+            <input id="searchTerm" type="text" ref={searchData} />
+          </div>
+
+          <button type="submit">Search</button>
         </form>
       </section>
-      <section>{post && <PostCard key={post._id} post={post} />}</section>
+      <section>
+        {post && (
+          <>
+            <PostCard key={post._id} post={post[0]} />
+            <Link to={`/update-editor/${post[0].slug}`}>Modify</Link>
+          </>
+        )}
+      </section>
     </main>
   );
 };
