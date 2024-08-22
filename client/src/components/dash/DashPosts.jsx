6394e29b2cdac8659f4278f238512c5b8ba5e7d7
ShowGa <peter1997217123@gmail.com>
2024-08-22 13:47:11 +0800

    <Progress> Client: Dashboard => DashPosts.jsx structure

--- client/src/components/dash/DashPosts.jsx
+++ client/src/components/dash/DashPosts.jsx
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+// css
+import "../components.css";
+// components
+import PostCard from "../card/PostCard";
 
 const DashPosts = () => {
-  return <div>DashPosts</div>;
+  const [post, setPost] = useState(null);
+
+  return (
+    <main className="max-w-[80%] mx-auto max-md:max-w-[95%] mt-12">
+      <section>
+        <form className="c-dash-search_form">
+          <label>Search Term :</label>
+          <input id="searchTerm" type="text" />
+        </form>
+      </section>
+      <section>{post && <PostCard key={post._id} post={post} />}</section>
+    </main>
+  );
 };
 
 export default DashPosts;
